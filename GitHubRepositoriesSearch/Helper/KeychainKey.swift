9e7c8558fc0d8b1a32a94f59be72becf56cf01de
tomoyasuzuki <tomoya.s0121@icloud.com>
2019-10-20 16:08:48 +0900

    refactor

---
+++ GitHubRepositoriesSearch/Helper/KeychainKey.swift
@@ -0,0 +1,12 @@
+//
+//  KeychainManager.swift
+//  GitHubRepositoriesSearch
+//
+//  Created by 鈴木友也 on 2019/10/18.
+//  Copyright © 2019 tomoya.suzuki. All rights reserved.
+//
+
+enum KeychainKey: String {
+    case email
+    case password
+}
